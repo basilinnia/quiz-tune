9df205066b6db04c7e1b4fa8b5a24c925753b406
Ecem Kaya <dev.ecemk01@gmail.com>
2025-07-02 00:39:14 +0300

    quiz page done

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -6,7 +6,7 @@ import Options from "@/components/Options";
 const customTheme = createTheme({
   card: {
     root: {
-      base: "rounded-xl shadow-xl drop-shadow-xl/80 dark:bg-yellow-500 bg-gray-400",
+      base: "rounded-xl shadow-xl drop-shadow-xl/80 dark:bg-[#e65c00] bg-[#e65c00]",
       children: "space-y-4 p-6",
     }
   },
@@ -16,10 +16,10 @@ const options = ["Die With a Smile - Bruno Mars", "Crazy in love - Pitbull", "Sh
 export default function Page() {
     return (
         <ThemeProvider theme={customTheme}>
-            <div className="bg-[url(../../public/backgrounds/3.png)] bg-cover bg-center h-screen flex flex-col w-full items-center">
+            <div className=" bg-[url(../../public/backgrounds/4.png)] bg-cover bg-center h-screen flex flex-col w-full items-center">
                 <Navbar/>
-                <Card className="w-1/2 mx-auto h-1/12 mb-8">
-                    <h1 className="font-bold text-xl text-center py-4">Name the song!</h1>
+                <Card className="w-1/2 mx-auto -my-4 h-1/12 mb-8">
+                    <h1 className="btext font-bold text-xl text-center py-4">Name the song!</h1>
                 </Card>
                 <div className="h-40 w-40 rounded-2xl bg-gray-800 shadow-2xl mb-16 flex justify-center items-center">
                     <AiFillSound  className="w-20 h-20 transition-transform duration-300 ease-in-out hover:scale-125"/>
--- src/components/Options.tsx
+++ src/components/Options.tsx
@@ -1,21 +1,33 @@
-import {Button} from "flowbite-react";
+import {Button, createTheme, ThemeProvider} from "flowbite-react";
+
+const customTheme = createTheme({
+  button: {
+    color: {
+      primary: "bg-[#ffea00] hover:bg-[#ffea00]",
+      secondary: "bg-blue-500 hover:bg-blue-600",
+    }
+  },
+});
 
 export default function Options({options}: { options: string[] }) {
     return (
-        <div className="flex flex-col w-full gap-4 px-18">
-            <div className="grid grid-cols-2 gap-x-[6vw] gap-y-7">
-                {options.map((option) => {
-                        return (
-                            <Button
-                                className="stext h-16 rounded-lg shadow-lg hover:bg-purple-700 transition-colors drop-shadow-xl/90 text-lg font-bold"
-                                color="pink"
-                            >
-                                {option}
-                            </Button>
-                        )
-                    }
-                )}
+        <ThemeProvider theme={customTheme}>
+            <div className="flex flex-col w-full gap-4 px-18">
+                <div className="grid grid-cols-2 gap-x-[6vw] gap-y-7">
+                    {options.map((option) => {
+                            return (
+                                <Button
+                                    className="stext h-16 rounded-lg shadow-lg hover:bg-[#9c7d0c] transition-colors drop-shadow-xl/90 text-lg text-black font-bold"
+                                    key={option}
+                                    color="primary"
+                                >
+                                    {option}
+                                </Button>
+                            )
+                        }
+                    )}
+                </div>
             </div>
-        </div>
+        </ThemeProvider>
     );
 }
